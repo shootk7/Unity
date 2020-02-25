ebd0adb8df365bb006b82e4803c823baa07384d6
shootk7 <yuta.shutoku@litalico.co.jp>
2020-02-25 18:29:59 +0900

    + ActionGame

---
+++ ActionGame/Assets/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posx = player.transform.position.x;
+        float posy = player.transform.position.y;
+        float posz = player.transform.position.z;
+        transform.position = new Vector3(posx, posy + 5, posz - 5);
+    }
+}
